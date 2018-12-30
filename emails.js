75b0a120cc32cd4ac905b4f09195ab2508b76c50
setoseng <setoseng@gmail.com>
2018-12-30 14:12:19 -0600

    Solution
    A solution to the email.js

--- emails.js
+++ emails.js
@@ -1,9 +1,11 @@
-var numUniqueEmails = function(emails) {
-  emails.forEach(function(e){
-    //e = e.split("@");
-    var newString = e.replace(/.+/, "");
-    console.log(newString);
-  });
+const numUniqueEmails = emails => {
+  const hasReceivedMail = {};
+  emails.forEach(email =>
+    hasReceivedMail[email.substring(0, (email.indexOf('+') + 1
+    || email.indexOf('@') + 1) - 1).replace(/\./g,'') + email.substring(email.indexOf('@'))] = true
+  );
+  console.log(Object.keys(hasReceivedMail).length);
+  console.log("Has Received Object: ", hasReceivedMail);
 };
 
 numUniqueEmails(["test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"]);
