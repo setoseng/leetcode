e2ec4118e77d11ff345bd1a869239d634bd96d52
Seto <setoseng@gmail.com>
2022-10-23 12:11:35 -0500

    Update palindromeNumbers.js

--- Programming Skills I/palindromeNumbers.js
+++ Programming Skills I/palindromeNumbers.js
@@ -15,7 +15,19 @@
 //   } else return false;
 // };
 
+var isPalindrome =  function(x) {
+  if (x < 0 || x % 10 == 0 && x !=0)  {
+    return false;
+  }
+  var t = 0;
+  while(x > t) {
+    t = t * 10 + x % 10;
+    x = Math.floor(x / 10);
+  }
+  return t == x || x == Math.floor(t/10);
+};
+
 console.log(isPalindrome(121));
-console.log(isPalindrome(-121));
+console.log(isPalindrome(123));
 console.log(isPalindrome(10));
 //isPalindrome(121);
