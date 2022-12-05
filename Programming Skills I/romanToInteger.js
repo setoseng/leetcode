3cc79fc7bec688bb5b551c1cf91796501de91cdf
Seto <setoseng@gmail.com>
2022-12-04 18:23:19 -0600

    Solved Roman Numeral to Integer

--- Programming Skills I/romanToInteger.js
+++ Programming Skills I/romanToInteger.js
@@ -3,5 +3,33 @@
  * @return {number}
  */
  var romanToInt = function(s) {
-    
+  const symbols = {
+    "I": 1,
+    "V": 5,
+    "X": 10, 
+    "L": 50,
+    "C": 100,
+    "D": 500,
+    "M": 1000
+  }
+  let value = 0;
+  for(let x = 0; x < s.length; x++){
+    console.log(symbols[s[x]]);
+    if(symbols[s[x]] < symbols[s[x+1]]) {
+      value -= symbols[s[x]]
+    } else {
+      value += symbols[s[x]]
+    }
+  }
+  return value;
 };
+
+//console.log(romanToInt('III'))
+//console.log(romanToInt('LVIII'))
+//console.log(romanToInt('MCMXCIV'))
+
+romanToInt('III')
+//romanToInt('LVIII')
+
+
+
